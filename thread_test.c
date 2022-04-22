5f753778bf872b6fc8675a05ce30de701b2e5082
Mohammad-Safari <msafari8051@gmail.com>
2022-04-23 03:27:07 +0430

    Test passing argument to threads

--- thread_test.c
+++ thread_test.c
@@ -8,18 +8,38 @@
 // because exit is not still modified
 // for thread compatibilty and result
 // in kernel panic
+// void print(void *args)
+// {
+//     while (1)
+//     {
+//         printf(1, "hello\n");
+//         sleep(100);
+//     }
+//     return;
+// }
+// int main()
+// {
+//     thread_creator(print, (void *)NULL);
+//     while (1)
+//         ;
+// }
+
+// TEST 2:
+// simple run "whitout exit" test by arg
 void print(void *args)
 {
+    printf(1, "hello from thread-%d\n", *(int *)args);
     while (1)
-    {
-        printf(1, "hello\n");
-        sleep(100);
-    }
+        ;
     return;
 }
 int main()
 {
-    thread_creator(print, (void *)NULL);
+    int i = 0;
     while (1)
-        ;
+    {
+        thread_creator(print, (void *)&i);
+        sleep(100);
+        i++;
+    }
 }
